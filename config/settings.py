948c684f7506dac8fc0cd4b0598d976edded3d06
Soheil Sirousi <alex.apple2403@gmail.com>
2025-07-04 21:26:39 +0330

    create news app

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news.apps.NewsConfig',
 
     # Third-Party apps
     'rest_framework',
